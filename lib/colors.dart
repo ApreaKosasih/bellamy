56d5718bf0ea9370673e23345090783ba2894ea2
Aprea <apreak@gmail.com>
2022-09-21 16:08:44 +0700

    color scheme

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  final black = const Color(0xFF1B1C1E);
+  final black1 = const Color(0xFF1F1F1F);
+  final black2 = const Color(0xFF2B2B2B);
+  final black3 = const Color(0xFF303030);
+  final white = const Color(0xFFf8f8f8);
+
+  const AppColors();
+}
+
+const colors = AppColors();
