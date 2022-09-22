e8ead0e5ab04fcea41fbb24e8ec3811b437b6a32
Aprea <apreak@gmail.com>
2022-09-22 13:12:32 +0700

    custom colorscheme

--- lib/bellamy.dart
+++ lib/bellamy.dart
@@ -9,6 +9,36 @@ class AppTheme {
 
   const AppTheme._();
 
+  static ThemeData custom({
+    Color? primary = Colors.blue,
+    Color? secondary = Colors.indigo,
+    Color? error = Colors.red,
+    Color? surface = Colors.white,
+    Color? background = Colors.white,
+    Brightness? brightness = Brightness.light,
+    Color? onPrimary = Colors.white,
+    Color? onSecondary = Colors.white,
+    Color? onError = Colors.white,
+    Color? onSurface = Colors.black,
+    Color? onBackground = Colors.black,
+  }) {
+    return ThemeData(
+      colorScheme: ColorScheme(
+        primary: primary!,
+        secondary: secondary!,
+        error: error!,
+        surface: surface!,
+        background: background!,
+        brightness: brightness!,
+        onPrimary: onPrimary!,
+        onSecondary: onSecondary!,
+        onBackground: onBackground!,
+        onError: onError!,
+        onSurface: onSurface!,
+      ),
+    );
+  }
+
   static ThemeData nature() {
     return ThemeData(colorScheme: _bellamyNature);
   }
